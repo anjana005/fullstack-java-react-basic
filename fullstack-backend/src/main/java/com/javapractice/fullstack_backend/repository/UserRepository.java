ab9a8fc87818ce8666c9724f403a5b6da147a9d4
Anjana <anjanaanjumk@gmail.com>
2024-07-27 20:05:06 +0530

    first commit

---
+++ fullstack-backend/src/main/java/com/javapractice/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.javapractice.fullstack_backend.repository;
+
+import com.javapractice.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
